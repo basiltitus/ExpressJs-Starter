ea07431bac2f1eb9a95da01aa21c46ea68cfa575
BasilTitus98 <basiltitus98@outlook.com>
2022-01-13 11:43:27 +0530

    added error handling

--- app.js
+++ app.js
@@ -3,8 +3,10 @@ const dotenv=require("dotenv")
 const v1=require("./routes/v1")
 const logger=require('./middlewares/logger')
 const morgan=require("morgan")
+const errorHandler=require("./middlewares/error")
 const app=express()
 app.use(express.json())
+
 const connectDb=require("./config/db")
 const connectDB = require("./config/db")
 dotenv.config({path:"./config/config.env"})
@@ -19,5 +21,6 @@ app.get("/",(req,res)=>{
 })
 
 app.use("/api/v1",v1);
+app.use(errorHandler);
 
 app.listen(process.env.PORT,console.log("App listening to "+process.env.PORT))
--- controllers/v1.js
+++ controllers/v1.js
@@ -1,3 +1,8 @@
-exports.getWelcomeMessage=(req,res)=>{
-    res.status(200).send("welcome to version 1")
+exports.getWelcomeMessage=(req,res,next)=>{
+    try{
+        throw new Error();
+        res.status(200).send("welcome to version 1a")
+    }catch(err){
+        next(err);
+    }
 };
